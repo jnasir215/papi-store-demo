0216ce697b1fe3b781e8ed473bfc15fc8e662e87
Jamal <jnasir247@gmail.com>
2020-11-20 15:27:18 -0500

    Added Comments to Indicate Comprehension of Coding

--- src/main/java/com/codedifferently/store/PapiStore.java
+++ src/main/java/com/codedifferently/store/PapiStore.java
@@ -1,67 +1,67 @@
-package com.codedifferently.store;
+package com.codedifferently.store; //Code Differently package
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList; //ArrayList Class imported from java.util package
+import java.util.HashMap;   //HashMap Class imported from java.util package
+import java.util.Map;       //Map Class imported from java.util package
 
-public class PapiStore {
-    private boolean isStoreOpen;
-    private ArrayList<String> tabs;
-    Map<String, Double> owesPapiMoney;
+public class PapiStore {                //PapiStore Class declaration (Part of the .store Code Differently package
+    private boolean isStoreOpen;        //Private boolean variable to determine whether or not the store is open
+    private ArrayList<String> tabs;     //Private arrayList to store new String variables
+    Map<String, Double> owesPapiMoney;  //Map variable to store who is available to have a tab and whether or not they owe money
 
-    public PapiStore(){
-        this.isStoreOpen = false;
-        this.tabs = new ArrayList();
-        this.owesPapiMoney = new HashMap<>();
+    public PapiStore(){                 //Constructor of PapiStore Class -> declares beginning state of the class
+        this.isStoreOpen = false;       //Declares the beginning state of the store as 'closed'
+        this.tabs = new ArrayList();    //Declares an pointer ArrayList Class for usage throughout
+        this.owesPapiMoney = new HashMap<>();   //Declares a pointer HashMap Class for usage throughout
     }
 
-    public PapiStore(Boolean isOpen, ArrayList<String> tabsIn){
-        this.isStoreOpen = isOpen;
-        this.tabs = tabsIn;
-        this.owesPapiMoney = new HashMap<>();
+    public PapiStore(Boolean isOpen, ArrayList<String> tabsIn){ //Second Constructor of PapiStore Class accepting incoming parameters
+        this.isStoreOpen = isOpen;      //Declares the beginning state of the store as 'open'
+        this.tabs = tabsIn;             //Declares an ArrayList variable for usage throughout
+        this.owesPapiMoney = new HashMap<>();   //Declares a pointer HashMap Class for usage throughout
     }
 
-    public void closeStore(){
+    public void closeStore(){           //Method to be called to 'close' the store
 
     }
 
-    public void openStore(){
-        this.isStoreOpen = true;
+    public void openStore(){            //Method to be called to 'open' the store
+        this.isStoreOpen = true;        //Declares the store as 'open for business'
     }
 
-    public boolean isStoreOpen() {
-        return isStoreOpen;
+    public boolean isStoreOpen() {      //Method to be called to return current state of store (open or closed?)
+        return isStoreOpen;             //Returns the current state of the store (open or closed?)
     }
 
 
-    public Boolean allowToUseTab(String name){
-        Boolean response = false;
-        if(hasAnActiveTab(name) && !hasAnOverDueBalance(name))
-            response = true;
-        return response;
+    public Boolean allowToUseTab(String name){  //Method to be called to see if the name being passed in is 'allowed to have a tab'
+        Boolean response = false;               //Default response to this method is "No, this person is 'not allowed to have a tab'"
+        if(hasAnActiveTab(name) && !hasAnOverDueBalance(name))  //If person has a tab and does not have an overdue balance
+            response = true;                    //...tells the program that this person 'is allowed to use a tab'
+        return response;                        //Returns whether or not the person is 'allowed to use a tab'
     }
 
-    private boolean hasAnOverDueBalance(String name){
-        if(!owesPapiMoney.containsKey(name))
-            return false;
-        Double money = owesPapiMoney.get(name);
-        Boolean owes = false;
-        if(money > 0.0)
-            owes = true;
-        return owes;
+    private boolean hasAnOverDueBalance(String name){   //Method to be called to see if the name being passed in 'has an overdue balance'
+        if(!owesPapiMoney.containsKey(name))    //If any item in the HashMap list does not match the name being passed into the method
+            return false;                       //...tells the program that this person 'does not have an overdue balance
+        Double money = owesPapiMoney.get(name); //Local variable that assigns a name to the fact that money is owed under this tab
+        Boolean owes = false;                   //Boolean variable that tells the program whether or not 'money is owed' - defaulted to 'No'
+        if(money > 0.0)                         //If dollar value is greater than zero
+            owes = true;                        //...tells the program that this person owes money
+        return owes;                            //Returns whether or not money is owed for this person
     }
 
-    private boolean hasAnActiveTab(String name){
-        for (String tab : tabs){
-            if (tab.equals(name))
-                return true;
+    private boolean hasAnActiveTab(String name){    //Method to be called to see if the name being passed in 'has an active tab'
+        for (String tab : tabs){                    //Loop to check through the list of Strings in the ArrayList
+            if (tab.equals(name))                   //If one of the Strings in the list is the same as the name being passed into the method
+                return true;                        //Method tells the program that the name given 'has an active tab'
         }
-        return false;
-    }
+        return false;                               //If it goes through the loop without a match, tells the program that the
+    }                                               //...name given does 'not have an active tab'
 
-    public void createActiveTab(String name){
-        this.tabs.add(name);
-    }
+    public void createActiveTab(String name){       //Method to be called when someone wants to start a tab
+        this.tabs.add(name);                        //Adds a name to the ArrayList of names (Strings) created
+    }                                                   //specifically for the currently constructed Class 'PapiStore'
 
 
     public static void main(String[] args) {
--- src/test/java/com/codedifferently/store/PapiStoreTest.java
+++ src/test/java/com/codedifferently/store/PapiStoreTest.java
@@ -1,63 +1,60 @@
-// This is the package it defines the family relation of objects
-package com.codedifferently.store;
+package com.codedifferently.store;  //This is the package it defines the family relation of objects
 
-// This is importing the Assert Class from the org.junit package or folder
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
-// This is a public class called Papi
-    public class PapiStoreTest {
+import org.junit.Assert;    //This is importing the Assert Class from the org.junit package or folder
+import org.junit.Before;    //This imports the Before Class from org.junit to allow to create a pseudo Constructor for testing purposes
+import org.junit.Test;      //This imports the Test Class from org.junit to allow the methods written to be recognized as 'tests'
 
-        PapiStore papiStore;
+    public class PapiStoreTest {        //This is a public class called Papi
+
+        PapiStore papiStore;            //papiStore variable declared of Object type PapiStore
 
         @Before
-        public void setup(){
-            papiStore = new PapiStore();
-            papiStore.createActiveTab("Jamal");
-            papiStore.createActiveTab("Victor");
-            papiStore.owesPapiMoney.put("Jamal", 0.0);
-            papiStore.owesPapiMoney.put("Victor", 7.0);
+        public void setup(){                //Test Constructor tailored specifically to the list of tests desired
+            papiStore = new PapiStore();    //papiStore variable declared to reference the Class PapiStore
+            papiStore.createActiveTab("Jamal"); //New tab created for the currently constructed PapiStore for Jamal
+            papiStore.createActiveTab("Victor");//New tab created for the currently constructed PapiStore for Victor
+            papiStore.owesPapiMoney.put("Jamal", 0.0);//Add Jamal to the list of those that have an active tab and how much he owes using the HashMap variable
+            papiStore.owesPapiMoney.put("Victor", 7.0);//Add Victor to the list of those that have an active tab and how much he owes using the HashMap variable
         }
 
         @Test
-        public void OpenStoreTest(){
+        public void OpenStoreTest(){    //Method designed to test whether or not the 'isStoreOpen' method works properly
             //Given
-
-            papiStore.openStore();
+            papiStore.openStore();      //Method called to open this currently constructed PapiStore
 
             //When
-            Boolean expected = true;
-            Boolean actual = papiStore.isStoreOpen();
-
+            Boolean expected = true;    //The 'expectation' is that the method works and will indicate that 'the store is open'
+            Boolean actual = papiStore.isStoreOpen();//What 'actually' happens when the method is called will be returned here
+                                                        //Did the method meet the expectation? 'Is it open' or 'is it closed'?
             //Then
             //Assert.assertEquals(expected, actual);
-            Assert.assertTrue(actual);
-        }
+            Assert.assertTrue(actual);  //Asserts that the method will meet the expectation that the store is open
+        }                                   //If so, this Test will PASS
 
         @Test
-        public void allowToUseTabTestSuccess(){
+        public void allowToUseTabTestSuccess(){ //Method designed to test whether or not the 'allowToUseTab' method works
             // given
-            PapiStore papiStore = new PapiStore();
+            PapiStore papiStore = new PapiStore();  //Local variable declared to reference the Class PapiStore
 
             // when
-            Boolean expected = true;
-            Boolean actual = papiStore.allowToUseTab("Jamal");
+            Boolean expected = true;            //The 'expectation' is that the method works and will successfully add the given name to the ArrayList
+            Boolean actual = papiStore.allowToUseTab("Jamal");  //What 'actually' happens when the method is called should add 'Jamal' to the ArrayList
 
             // then
-            Assert.assertEquals(expected, actual);
-        }
+            Assert.assertEquals(expected, actual);  //Asserts that the method will meet the expectation and will return a value of true
+        }                                               //If so, this Test will PASS
 
         @Test
-        public void allowToUseTabTestFail(){
+        public void allowToUseTabTestFail(){    //Method designed to test whether or not the 'allowToUseTab' method will predictably fail
             // given
-            PapiStore papiStore = new PapiStore();
+            PapiStore papiStore = new PapiStore();  //Local variable declared to reference the Class PapiStore
 
             // when
-            Boolean expected = false;
-            Boolean actual = papiStore.allowToUseTab("Victor");
+            Boolean expected = false;           //The 'expectation' is that the method will unsuccessfully add the given name to the ArrayList
+            Boolean actual = papiStore.allowToUseTab("Victor"); //What 'actually' happens when the method is called should add 'Victor' to the ArrayList
 
             // then
-            Assert.assertEquals(expected, actual);
-        }
+            Assert.assertEquals(expected, actual);  //Asserts that the method will meet the expectation and will return a value of false
+        }                                               //If the 'allowToUseTab' method works, this Test will FAIL
 }
